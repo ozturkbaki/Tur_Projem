9888a2b035b18558d2eb162b56e331d63f938301
ozturkbaki <ozturkabdulbaki@gmail.com>
2020-03-01 15:45:32 +0300

    Başlıklar Türkçeleştirildi ve siyah renk atandı

--- hakkinda.php
+++ hakkinda.php
@@ -27,27 +27,25 @@
   ?>
 
   <!-- Page Content -->
-  <div class="container">
+  <div style="color: black;" class="container">
 
     <!-- Page Heading/Breadcrumbs -->
-    <h1 class="mt-4 mb-3">About
-      <small>Subheading</small>
-    </h1>
+    <h1 class="mt-4 mb-3">Hakkımızda</h1>
 
-    <ol class="breadcrumb">
+    <ol  class="breadcrumb">
       <li class="breadcrumb-item">
-        <a href="index.html">Home</a>
+        <a style="color: black;"  href="index.php">Anasayfa</a>
       </li>
-      <li class="breadcrumb-item active">About</li>
+      <li  class="breadcrumb-item active">Hakkımızda</li>
     </ol>
 
     <!-- Intro Content -->
-    <div class="row">
+    <div style="color: black;" class="row">
       <div class="col-lg-6">
         <img class="img-fluid rounded mb-4" src="http://placehold.it/750x450" alt="">
       </div>
       <div class="col-lg-6">
-        <h2>About Modern Business</h2>
+        <h2 style="color: black;">Misyonumuz</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed voluptate nihil eum consectetur similique? Consectetur, quod, incidunt, harum nisi dolores delectus reprehenderit voluptatem perferendis dicta dolorem non blanditiis ex fugiat.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, magni, aperiam vitae illum voluptatum aut sequi impedit non velit ab ea pariatur sint quidem corporis eveniet. Odit, temporibus reprehenderit dolorum!</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, consequuntur, modi mollitia corporis ipsa voluptate corrupti eum ratione ex ea praesentium quibusdam? Aut, in eum facere corrupti necessitatibus perspiciatis quis?</p>
