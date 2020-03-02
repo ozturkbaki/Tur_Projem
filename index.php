04e74d9f8d5720fe9d0edf2817bcf054238f16dd
ozturkbaki <ozturkabdulbaki@gmail.com>
2020-03-02 20:56:30 +0300

    açıklama satırı silindi

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
   <title>Pro-Tur</title>
 
-  <!-- Bootstrap core CSS -->
+  
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
   <!-- Custom styles for this template -->
